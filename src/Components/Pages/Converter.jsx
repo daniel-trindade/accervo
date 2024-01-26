eaca78007b9b41a139dda9344d378b80c0ff1f50
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-01-25 23:52:17 -0300

    revolvendo

--- src/Components/Pages/Converter.jsx
+++ src/Components/Pages/Converter.jsx
@@ -3,29 +3,29 @@ import Length from "../Converters/Length"
 
 function Converter(){
 
-  const units = ["Selecione uma Opção", "Comprimento", "Volume", "Temperatura"];
+  const units = ["Selecione uma Opção", "Comprimento", "Volume", "Temperatura", "Arroz"];
   const optUnit = document.getElementById("unitSelector")
 
-  const options = document.createElement("optgroup")
-  options.setAttribute('label', 'units')
-  units.forEach(function(unt){
+  window.addEventListener('load', async ()=>{
+    const options = document.createElement("optgroup")
+
+    options.setAttribute('label', 'units')
+
+    units.forEach(function(unt){
     options.innerHTML += '<option>' + unt + '</option>'
+    })
+
+    optUnit.append(options)
   })
 
-  function selector(){
-    optUnit.append(options)
-  }
+
 
   return(
     <div>
       <h1>Conversor de Unidades de Medida</h1>
       <p>Aqui você pode selecionar o conversor, unidade de partida e unidade desejada usando as caixas a baixo:</p>
       <p>Unidade de Medida:</p>
-      <select name="unitSelector" id="unitSelector" className={styles.selectBox} onLoad={selector}>
-        <option value="Comprimento">Comprimento</option>
-        <option value="Volume">Volume</option>
-        <option value="Temperatura">Temperatura</option>
-      </select>
+      <select name="unitSelector" id="unitSelector" className={styles.selectBox}></select>
       <Length/>
     </div>
   )
